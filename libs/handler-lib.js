9eb7100f8afdc0920ed42e181fcc35fb7fc55911
Santosh9009 <patisantosh00@gmail.com>
2023-06-07 21:27:22 +0530

    second

---
+++ libs/handler-lib.js
@@ -0,0 +1,19 @@
+export default function handler(lambda) {
+  return async function (event, context) {
+    let body, statusCode;
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      body = { error: e.message };
+      statusCode = 500;
+    }
+    // Return HTTP response
+    return {
+     statusCode,
+     body: JSON.stringify(body),
+   };
+ };
+}
